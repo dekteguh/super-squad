9fbe1deff8ff6a011dec8b61462d1b40c9769ab8
Eko Teguh Widodo <dekteguh@gmail.com>
2017-08-15 16:14:28 +0700

    Styling....

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import CharacterList from './CharacterList';
 
+import '../styles/index.css';
+
 class App extends Component {
 
     render() {
         return (
-            <div>
+            <div className="App">
                 <h1>Super Squad</h1>
                 <CharacterList />
             </div>    
